99905bcca7bb076083e3bc986ef71fd89d8df3d0
Hyunmin Choi <127882281+hchoi3@users.noreply.github.com>
2023-03-17 16:29:48 -0400

    Update main.tf

--- ECR/main.tf
+++ ECR/main.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "ecr" {
-  name                 = "antraregistry"
+  name                 = "Myregistry"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
