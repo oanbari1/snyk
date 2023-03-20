0274db17353e2cb6fc38d0d5634ae5cbda6b9a99
hchoi3 <hyunmin.choi@axleinfo.com>
2023-03-20 16:23:39 -0400

    ecr name changed

--- Terraform/ECR/main.tf
+++ Terraform/ECR/main.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "ecr" {
-  name                 = "Myregistry"
+  name                 = "myregistry"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
