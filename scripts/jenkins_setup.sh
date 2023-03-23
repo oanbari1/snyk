da229be295751602af07d65cd1d0ad195e0dcc59
Hyunmin Choi <127882281+hchoi3@users.noreply.github.com>
2023-03-23 15:40:52 -0400

    Update jenkins_setup.sh

--- scripts/jenkins_setup.sh
+++ scripts/jenkins_setup.sh
@@ -4,7 +4,7 @@
 sudo apt install openjdk-11-jdk -y
 
 #add repository key to system 
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key |sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
 
 #append repo to sources.list
 sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
